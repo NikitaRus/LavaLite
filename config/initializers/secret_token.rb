b6583fcc3e38d346d0b666c6e75fc8d6c0ccda3c
Nicolas Oga <2112.oga@gmail.com>
2011-10-02 19:34:06 -0300

    Prototype

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LavaLite::Application.config.secret_token = '4155d4423e02318ad1a1d650b34498205d215547c3c68a2fdde8377ab96b086c94088dc615ba309829ee71f5b927383ade8102037e24e3b5356904e3fc109744'
